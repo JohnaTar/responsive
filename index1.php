06876f1dbdccb2d880714c0c116144190e324192
JohnaTar <leavemetar@gmail.com>
2017-05-06 15:16:12 +0700

    finish

--- index1.php
+++ index1.php
@@ -22,13 +22,22 @@
 
     <!-- Custom styles for this template -->
     <link href="css_2/sb-admin.css" rel="stylesheet">
+<!-- รูป กระพรืบ /////////////////////////////// -->
+    <script src="js/wow.min.js"></script>
+    <link rel="stylesheet" href="css/animate.css">
+    <script>new WOW().init();</script>
+<!-- ************/////////////////////////////// -->
+<script src="https://use.fontawesome.com/9a1dedc2b2.js"></script>
+
 
 </head>
 
-<body>
+<body  data-spy="scroll" data-target=".navbar" data-offset="50">
+
+<?php include('includes/func.php'); ?>
 
     <!-- Navigation -->
-    <nav id="mainNav" class="navbar static-top navbar-toggleable-md navbar-light bg-default" >
+    <nav id="mainNav" class="navbar  static-top navbar-toggleable-md navbar-light bg-default" style="box-shadow: 0 0 3px 3px #EEEEEE; v " >
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -102,9 +111,10 @@
             </ul>
         </div>
     </nav>
+<div style=" padding-top:150px;"> </div>
 
    <!--  ////////////////////////////PICTURE ////////////////////////////////////////////////////////-->
-    <div class="content-wrapper py-3">
+    <div class="content-wrapper py-3" >
 
         <div class="container">
      
@@ -116,10 +126,10 @@
            <div class="col-md-4 " style="text-align: center; padding-top: 25px">
            
                             <a href="#">
-                                <img  class="card-img-top img-fluid" src="images/icon_expert.png" width="100"  alt="">
+                                <img  class="wow flipInY" data-wow-offset="200" src="images/icon_expert.png" width="100"  alt="">
                             </a>
                             <div class="card-block" style="padding-top: 45px">
-                                <h6 class="card-title mb-1"><a href="#">500 Expert</a></h6>
+                                <h6 class="card-title mb-1">500 Expert</h6>
                                
                             </div>
                            
@@ -129,10 +139,10 @@
               <div class="col-md-4" style="text-align: center; ">
            
                             <a href="#">
-                                <img class="card-img-top img-fluid " src="images/sgd.jpg" width="150" alt="">
+                                <img  class="wow flipInY" data-wow-offset="200" src="images/sgd.jpg" width="150" alt="">
                             </a>
                             <div class="card-block">
-                                <h6 class="card-title mb-1"><a href="#">400 Institute</a></h6>
+                                <h6 class="card-title mb-1">400 Institute</h6>
                                
                             </div>
                            
@@ -144,10 +154,10 @@
               <div class="col-md-4" style="text-align: center; padding-top: 25px">
            
                             <a href="#">
-                                <img class="card-img-top img-fluid" width="100" src="images/Research.jpg" alt="">
+                                <img  class="wow flipInY" data-wow-offset="200" width="100" src="images/Research.jpg" alt="">
                             </a>
                             <div class="card-block" style="padding-top: 45px">
-                                <h6 class="card-title mb-1"><a href="#">100,000 Published</a></h6>
+                                <h6 class="card-title mb-1">100,000 Published</h6>
                                 
                             </div>
                             
@@ -158,10 +168,10 @@
                <div class="col-md-4" style="text-align: center; padding-top: 25px">
            
                             <a href="#">
-                                <img class="card-img-top img-fluid" width="100" src="images/mcsp-icon-malaria-v2.png" alt="">
+                                <img  class="wow flipInY" data-wow-offset="200" width="100" src="images/mcsp-icon-malaria-v2.png" alt="">
                             </a>
                             <div class="card-block" style="padding-top: 45px">
-                                <h6 class="card-title mb-1"><a href="#">100,000 Published</a></h6>
+                                <h6 class="card-title mb-1">100,000 Published</h6>
                                 
                             </div>
                             
@@ -172,10 +182,10 @@
                <div class="col-md-4" style="text-align: center; padding-top: 25px">
            
                             <a href="#">
-                                <img class="card-img-top img-fluid" width="100" src="images/icon-vacunas.png" alt="">
+                                <img  class="wow flipInY" data-wow-offset="200" width="100" src="images/icon-vacunas.png" alt="">
                             </a>
                             <div class="card-block" style="padding-top: 45px">
-                                <h6 class="card-title mb-1"><a href="#">100,000 Published</a></h6>
+                                <h6 class="card-title mb-1">100,000 Published</h6>
                                 
                             </div>
                             
@@ -186,10 +196,10 @@
                <div class="col-md-4" style="text-align: center; padding-top: 25px">
            
                             <a href="#">
-                                <img class="card-img-top img-fluid" width="100" src="images/collaboration.png" alt="">
+                                <img  class="wow flipInY" data-wow-offset="200" width="100" src="images/collaboration.png" alt="">
                             </a>
                             <div class="card-block" style="padding-top: 45px">
-                                <h6 class="card-title mb-1"><a href="#">100,000 Published</a></h6>
+                                <h6 class="card-title mb-1">100,000 Published<!--ADD LINL <a href="#">100,000 Published</a> --></h6>
                                 
                             </div>
                             
@@ -198,119 +208,87 @@
                 
                 
                 
-    </div> <!-- /////////////row -->
+    </div> 
+        <!-- /////////////row ////////////////////////////////////////////////////////////////////////////-->
+        <div style=" padding-top:10px;"> </div>
 
-
-       <!--          <div class="card mb-3">
-                <div class="card-header">
-                    <i class="fa fa-area-chart"></i> Area Chart Example
-                </div>
-                <div class="card-block">
-                <table width="90%" style=" padding-top:10px;" align="center">
-            <tr>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/icon_expert.png" width="100" ></td>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/sgd.jpg" width="150" ></td>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/Research.jpg" width="100" ></td>
-                <td align="center"></td>
-            </tr>
-            <tr><td><br></td></tr>
-            <tr>
-                                <td align="center"><div class="number-expert"><b>500 Expert </div></td><td align="center"><b>400 Institute</td><td align="center"><b>100,000 Published </td>
-                        </tr>
-             <tr>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/mcsp-icon-malaria-v2.png" width="100" ></td>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/icon-vacunas.png" width="100" ></td>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/collaboration.png" width="100" ></td>
-                                <td align="center"></td>
-                        </tr>
-                        <tr><td><br></td></tr>
-                        <tr>
-                                <td align="center"><div class="number-expert"><b>xxx Malaria or Dengue</div></td><td align="center"><b>xxx Vaccine</td><td align="center"><b>xxx Collaborative </td>
-                        </tr>
-
-        </table>
-                </div>
-                <div class="card-footer small text-muted">
-                    Updated yesterday at 11:59 PM
-                </div>
+    <div class="row" style="border:1px solid #CCCCCC;box-shadow: 0 0 3px 3px #EEEEEE; ">
+         <div class="col-md-4" style="text-align: center; padding-top: 10px">
+            <div class="card-block" style="padding-top: 45px">
+                    <h6 class="card-title mb-1">Vaccine Published<!-- ADD LINK <a href="#">Vaccine Published</a> --></h6>
+                    <?php echo  feedpub('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/erss.cgi?rss_guid=1PW-O5V9EIrhHz8eAOQ1hY9LKSoOEVGgxZlEhZP7FAhG_OpM_e','Pubmed'); 
+                    ?>
+                                
             </div>
-        -->
-     
+   
+         </div>
+   <!--  ////////////////////////////////////////////////////////////////////////////////////////////////// -->
+
+         <div class="col-md-4" style="text-align: center; padding-top: 10px">
+              <div class="card-block" style="padding-top: 45px">
+                    <h6 class="card-title mb-1">Diseases Outbreak News<!-- ADD LINK<a href="#">Diseases Outbreak News</a>  --></h6>
+                    <?php echo  feednews('http://www.who.int/feeds/entity/csr/don/en/rss.xml'); ?> 
+                   
+                                
+            </div>
+         </div>
+      <!--  ////////////////////////////////////////////////////////////////////////////////////////////////// -->    
+         <div class="col-md-4" style="text-align: center; padding-top: 10px">
+             <div class="card-block" style="padding-top: 45px">
+                    <h6 class="card-title mb-1">Recent update<!-- ADD LINK <a href="#">Recent update</a> --></h6>
+                    
+                   
+                                
+            </div>
+        
+         </div>
+
+
+
+ 
+    </div>    <!-- /////////////row //////////////////////////////////////////////////////////////////-->
+
+
+   
+
+
  
                 
-</div>
-
-
-           <!--  <div style=" padding-top:100px;"> </div>
-<table width="80%"  border="0" align="center" cellpadding="0" cellspacing="0" style="border:1px solid #CCCCCC;box-shadow: 0 0 3px 3px #EEEEEE; ">
-<tr  heigth="800px">
-    <td bgcolor="#FFFFFF" style="vertical-align:top; ">
-        <table width="90%" style=" padding-top:10px;" align="center">
-            <tr>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/icon_expert.png" width="100" ></td>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/sgd.jpg" width="150" ></td>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/Research.jpg" width="100" ></td>
-                <td align="center"></td>
-            </tr>
-            <tr><td><br></td></tr>
-            <tr>
-                                <td align="center"><div class="number-expert"><b>500 Expert </div></td><td align="center"><b>400 Institute</td><td align="center"><b>100,000 Published </td>
-                        </tr>
-             <tr>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/mcsp-icon-malaria-v2.png" width="100" ></td>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/icon-vacunas.png" width="100" ></td>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/collaboration.png" width="100" ></td>
-                                <td align="center"></td>
-                        </tr>
-                        <tr><td><br></td></tr>
-                        <tr>
-                                <td align="center"><div class="number-expert"><b>xxx Malaria or Dengue</div></td><td align="center"><b>xxx Vaccine</td><td align="center"><b>xxx Collaborative </td>
-                        </tr>
-
-        </table> -->
-
-<!-- 
-<tr><td  bgcolor="#FFFFFF" style="vertical-align:top; " align="center"><br></td></tr>
-
-<table width="80%"  border="0" align="center" cellpadding="0" cellspacing="0" style="border:1px solid #CCCCCC;box-shadow: 0 0 3px 3px #EEEEEE; ">
-<tr  heigth="800px">
-    <td bgcolor="#FFFFFF" style="vertical-align:top; ">
-        <table width="90%" style=" padding-top:10px;" align="center">
-            <tr>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/icon_expert.png" width="100" ></td>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/sgd.jpg" width="150" ></td>
-                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/Research.jpg" width="100" ></td>
-                <td align="center"></td>
-            </tr>
-            <tr><td><br></td></tr>
-            <tr>
-                                <td align="center"><div class="number-expert"><b>500 Expert </div></td><td align="center"><b>400 Institute</td><td align="center"><b>100,000 Published </td>
-                        </tr>
-             <tr>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/mcsp-icon-malaria-v2.png" width="100" ></td>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/icon-vacunas.png" width="100" ></td>
-                                <td align="center"><img class="wow flipInY" data-wow-offset="200" src="images/collaboration.png" width="100" ></td>
-                                <td align="center"></td>
-                        </tr>
-                        <tr><td><br></td></tr>
-                        <tr>
-                                <td align="center"><div class="number-expert"><b>xxx Malaria or Dengue</div></td><td align="center"><b>xxx Vaccine</td><td align="center"><b>xxx Collaborative </td>
-                        </tr>
-
-        </table>
-
- -->
-
-
-
-            <!-- Breadcrumbs -->
-         
-        </div>
+        </div> <!-- //Contrainer*/////////////////////////////////////////////////////////////// -->
+      
+     </div>
         <!-- /.container-fluid -->
+<!-- 
+////////////////////////////////////////////////////////////////////////// -->
+<style type="text/css">
+    .footer-bottom {
+ background:#444;
+ padding:30px 0;
+ border-top:1px solid #d9d9d9;
+ font-size:16px;
+ color:white;
+}
+</style>
+<div class="footer-bottom">
+    <div class="container">
+        <div class="row">
 
+            
+            <div class="col-md-12 widget">
+    <center>
+        <a href="https://www.facebook.com/nvikm/?fref=ts"> <i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a> :
+        <a href="https://twitter.com/nvikm"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a> :
+        <a href="https://www.google.co.th/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwiwhbDUkP3SAhVBto8KHfCzCskQFggaMAA&url=https%3A%2F%2Fth.foursquare.com%2Fv%2F%25E0%25B8%25AA%25E0%25B8%2596%25E0%25B8%25B2%25E0%25B8%259A%25E0%25B8%2599%25E0%25B8%25A7%25E0%25B8%2584%25E0%25B8%258B%25E0%25B8%2599%25E0%25B9%2581%25E0%25B8%25AB%25E0%25B8%2587%25E0%25B8%258A%25E0%25B8%25B2%25E0%25B8%2595-%25E0%25B8%25AD%25E0%25B8%2587%25E0%25B8%2584%25E0%25B8%2581%25E0%25B8%25B2%25E0%25B8%25A3%25E0%25B8%25A1%25E0%25B8%25AB%25E0%25B8%25B2%25E0%25B8%258A%25E0%25B8%2599%2F5236959011d2054cfbeba341&usg=AFQjCNGylkaQERSguab76zpopS9HEGHAIg&sig2=8oLVl2A-n2SEIwyKxi-W6A"><i class="fa fa-foursquare fa-2x" aria-hidden="true"></i></a>
+    </center>  
+        <br>
+        Department of Computer Science,Faculty of Science and Technology.&copy;2017 Rajamangala University of Technology Suvarnabhumi.. All Rights Reserved.
+        </br>
+
+            <center> Powered by Department of Computer Science,Faculty of Science and Technology @ RmutSB »</center>
+            </div>
+        </div>
     </div>
-    <!-- /.content-wrapper -->
-
+</div>
     
 
     <!-- Bootstrap core JavaScript -->
